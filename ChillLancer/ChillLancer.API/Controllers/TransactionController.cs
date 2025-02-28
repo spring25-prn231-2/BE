24557243825f7c83b5a0572d36237d8c37417278
Totr <minhtoan1696@gmail.com>
2025-03-01 02:32:33 +0700

    crud project + transaction service

---
+++ ChillLancer/ChillLancer.API/Controllers/TransactionController.cs
@@ -0,0 +1,24 @@
+﻿using ChillLancer.BusinessService.BusinessModels.Transaction;
+using ChillLancer.BusinessService.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChillLancer.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : Controller
+    {
+        private readonly ITransactionService _transactionService;
+        private readonly IAccountService _accountService;
+
+        public TransactionController(
+            ITransactionService transactionService,
+            IAccountService accountService
+            )
+        {
+            _transactionService = transactionService;
+            _accountService = accountService;
+        }
+    }
+}
