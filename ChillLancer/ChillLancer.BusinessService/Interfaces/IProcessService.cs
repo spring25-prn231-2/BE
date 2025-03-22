2535292b2152abaa9bcdace9667777aae4365186
ToanTran5 <132196011+ToanTran5@users.noreply.github.com>
2025-03-22 13:11:16 +0700

    Revert "Toan"

--- ChillLancer/ChillLancer.BusinessService/Interfaces/IProcessService.cs
+++ ChillLancer/ChillLancer.BusinessService/Interfaces/IProcessService.cs
@@ -4,16 +4,6 @@ namespace ChillLancer.BusinessService.Interfaces
 {
     public interface IProcessService
     {
-<<<<<<< HEAD
-        Task<bool> Add(List<ProcessCreateBM> inputData);
-        Task<List<ProcessBM>> GetAll();
-        Task<ProcessBM> GetProcessById(Guid id);
-        Task<bool> DeleteProcess(Guid id);
-        Task<bool> UpdateProcess(ProcessBM process);
-        Task<bool> SubmitTask(Guid id, TaskSubmissionModel model);
-        Task<bool> UpdateStatus(string status, Guid id);
-=======
->>>>>>> a66a21bd2063b3cca3d8475db107af1341883cf9
         //Task<bool> Add(List<ProcessBM> inputData);
         Task<bool> Update(List<ProcessUpdateBM> inputData, Guid proposalId);
         Task<bool> Delete(List<ProcessUpdateBM> inputData, Guid proposalId);
